c0a38ea6a96024a2c4dc41db84b7d0961e38e6b0
JFresnedaFuentespina <jfresnedafuentespina@gmail.com>
2026-03-11 11:21:45 +0100

    Add GAgent

--- Assets/Scripts/GAgent.cs
+++ Assets/Scripts/GAgent.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Subgoal
+{
+    public Dictionary<string, int> sgoals;
+    public bool remove;
+
+    public Subgoal(string s, int i, bool r)
+    {
+        sgoals = new Dictionary<string, int>();
+        sgoals.Add(s, i);
+        remove = r;
+    }
+
+}
 
 public class GAgent : MonoBehaviour
 {
+    public List<GAction> actions = new List<GAction>();
+    public Dictionary<Subgoal, int> goals = new Dictionary<Subgoal, int>();
+
+    GPlanner planner;
+    Queue<GAction> actionQueue;
+    public GAction currentAction;
+    Subgoal currentGoal;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GAction[] acts = GetComponents<GAction>();
+        foreach(GAction a in acts)
+        {
+            actions.Add(a);
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+
     }
 }
